4524d5b59cca4a8c9be44813c0d74890e5e6a47e
joe Defendre <jchacker5@gmail.com>
2023-09-14 13:58:50 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#this is a comment
+
+print("Hello World")
+
+message: str = "this is a string" #this is a typehint
+print(message)
+# everything is an object in python
+cap_message = message.capitalize()
+print(cap_message)
+
+number = 23 #this is an int
+print(type(number)) #this is how to get the type of the variable
+print(number)
+
+decimal = 5.8 #float is a clas in python 
+print(type(decimal))
+
+boolean = True #boolean is a class in python
+print(type(boolean))
+print(f'This is a  number: {34 + 6} and this is a string: {message}')
+
+python_list = [1, 6.7, "joe", True, 4, 5] #this is a list with snake_case
